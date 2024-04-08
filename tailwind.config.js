314f0ba7eca1278e56b49a09323feb41484d1fae
InoVin19 <rey_vincent_inocencio19@gmail.com>
2024-04-09 01:19:11 +0800

    Created Main Page for Transaction Management

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.{html,js}"],
+  content: ["./views/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'Poppins': ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+} 
