a9a3e4bcbbcffa2b5aaa368436cd3d619ae9c633
aaronjardenil <aaron_randall_jardenil@dlsu.edu.ph>
2024-04-09 17:25:30 +0800

    separated into async functions

--- app.js
+++ app.js
@@ -10,6 +10,51 @@
 // const [result] = await pool.query("SELECT * FROM test_db LIMIT 1")
 // console.log(result[0])
 
+/*
+
+    CODE DUMP
+
+    app.get('/add', async (req, res) => {
+    try {
+        const data = 1;
+        const secondaryNodeConnection = await util.promisify(secondaryNode.getConnection).bind(secondaryNode)();
+        const query = util.promisify(secondaryNodeConnection.query).bind(secondaryNodeConnection);
+        
+        // Start transaction
+        var log = { message: 'Starting Transaction', timestamp: new Date() };
+        addToLog(log);
+        console.log(log);
+        await query('START TRANSACTION');
+        
+        const result = await query('INSERT INTO test_table (test_column) VALUES (?)', (data));
+        log = { message: 'Inserting', timestamp: new Date() };
+        addToLog(log);
+        console.log('Inserting data:', data);
+        
+        // Commit transaction
+        console.log('Committing transaction');
+        await query('COMMIT');
+        log = { message: 'Committing Transaction', timestamp: new Date() };
+        addToLog(log);
+        
+        secondaryNodeConnection.release();
+        
+        if (result.affectedRows === 1) {
+            console.log('Data inserted successfully');
+            res.status(200).json({ message: 'Data inserted successfully' });
+        } else {
+            console.log('Failed to insert data');
+            res.status(500).json({ message: 'Failed to insert data' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+    console.log('Query uncommitted');
+    });
+
+*/
+
 // IMPORTS
 
 import util from 'util';
@@ -37,7 +82,16 @@ const centralNode = createPool({
 });
 
 // visayas-mindanao database connection
-const secondaryNode = createPool({
+const luzonNode = createPool({
+    connectionLimit: 10,
+    host: 'ccscloud.dlsu.edu.ph',
+    port: '20043',
+    user: 'root',
+    password: '12345678',
+    database: 'test_schema'
+});
+
+const visayasMindanaoNode = createPool({
     connectionLimit: 10,
     host: 'ccscloud.dlsu.edu.ph',
     port: '20044',
@@ -46,6 +100,31 @@ const secondaryNode = createPool({
     database: 'test_schema'
 });
 
+// APP ACTION FUNCTIONS
+
+async function testConnections() {
+    // test connection to centralNode pool
+    const centralNodeConnection = util.promisify(centralNode.getConnection).bind(centralNode);
+    try {
+        const connection = await centralNodeConnection();
+        console.log('Connected to centralNode pool');
+        connection.release();
+    } catch (err) {
+        console.error('Error connecting to centralNode pool:', err);
+    }
+
+    // test connection to secondaryNode pool
+    const secondaryNodeConnection = util.promisify(secondaryNode.getConnection).bind(secondaryNode);
+    try {
+        const connection = await secondaryNodeConnection();
+        console.log('Connected to secondaryNode pool');
+        connection.release();
+    } catch (err) {
+        console.error('Error connecting to secondaryNode pool:', err);
+    }
+}
+
+// * add options to write to different log files (we need 3, one for each database)
 function addToLog(log) {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
@@ -78,39 +157,165 @@ function addToLog(log) {
     });
 }
 
-// Example usage
+//
+async function addDataToTable(req, res, node) {
+  //const data = req.body;   **CHANGE IT TO THIS ONE WHEN THE TIME COMES
+    const data = 1;
 
-// APP ACTION FUNCTIONS
-
-async function testConnections() {
-    // test connection to centralNode pool
-    const centralNodeConnection = util.promisify(centralNode.getConnection).bind(centralNode);
-    try {
-        const connection = await centralNodeConnection();
-        console.log('Connected to centralNode pool');
-        connection.release();
-    } catch (err) {
-        console.error('Error connecting to centralNode pool:', err);
+    if(node == 'central') {
+        const nodeConnection = await util.promisify(centralNode.getConnection).bind(secondaryNode)();
+    }
+    else if(node == 'visayas-mindanao') {
+        const nodeConnection = await util.promisify(secondaryNode.getConnection).bind(secondaryNode)();
+    }
+    else {
+        const nodeConnection = await util.promisify(secondaryNode.getConnection).bind(secondaryNode)();
     }
 
-    // test connection to secondaryNode pool
-    const secondaryNodeConnection = util.promisify(secondaryNode.getConnection).bind(secondaryNode);
-    try {
-        const connection = await secondaryNodeConnection();
-        console.log('Connected to secondaryNode pool');
-        connection.release();
-    } catch (err) {
-        console.error('Error connecting to secondaryNode pool:', err);
+    const query = util.promisify(nodeConnection.query).bind(nodeConnection);
+
+    // add query to set isolation level at some point
+
+    // start transaction
+    await query('START TRANSACTION');
+
+    // log start of transaction
+    var log = { message: 'Starting Transaction', timestamp: new Date() };
+    addToLog(log);
+    console.log(log);
+
+    // ** EDIT QUERY WHEN THE TIME COMES
+    const result = await query('INSERT INTO test_table (test_column) VALUES (?)', (data));
+
+    // log query
+    log = { message: 'Inserting', timestamp: new Date() };
+    addToLog(log);
+    console.log('Inserting data:', data);
+
+    // commit transaction
+    console.log('Committing transaction');
+    await query('COMMIT');
+
+    // log commit
+    log = { message: 'Committing Transaction', timestamp: new Date() };
+    addToLog(log);
+
+    // release connection when finished
+    nodeConnection.release();
+
+    if (result.affectedRows === 1) {
+        console.log('Data inserted successfully');
+        res.status(200).json({ message: 'Data inserted successfully' });
+    } else {
+        console.log('Failed to insert data');
+        res.status(500).json({ message: 'Failed to insert data' });
+    }
+}
+
+// UPDATE APPOINTMENT
+async function updateDataInTable(req, res, node, id, data) {
+    if (node === 'central') {
+        const nodeConnection = await util.promisify(centralNode.getConnection).bind(centralNode)();
+    } else if (node === 'visayas-mindanao') {
+        const nodeConnection = await util.promisify(visayasMindanaoNode.getConnection).bind(visayasMindanaoNode)();
+    } else {
+        const nodeConnection = await util.promisify(luzonNode.getConnection).bind(luzonNode)();
+    }
+
+    const query = util.promisify(nodeConnection.query).bind(nodeConnection);
+
+    await query('START TRANSACTION');
+
+    const log = { message: 'Starting Transaction', timestamp: new Date() };
+    addToLog(log);
+    console.log(log);
+
+    const result = await query('UPDATE test_table SET test_column = ? WHERE id = ?', [data, id]);
+
+    log = { message: 'Updating', timestamp: new Date() };
+    addToLog(log);
+    console.log('Updating data:', data);
+
+    await query('COMMIT');
+
+    console.log('Committing transaction');
+    log = { message: 'Committing Transaction', timestamp: new Date() };
+    addToLog(log);
+
+    nodeConnection.release();
+
+    if (result.affectedRows === 1) {
+        console.log('Data updated successfully');
+        res.status(200).json({ message: 'Data updated successfully' });
+    } else {
+        console.log('Failed to update data');
+        res.status(500).json({ message: 'Failed to update data' });
+    }
+}
+
+// DELETE APPOINTMENT
+async function deleteDataFromTable(req, res, node, id) {
+    if (node === 'central') {
+        const nodeConnection = await util.promisify(centralNode.getConnection).bind(centralNode)();
+    } else if (node === 'visayas-mindanao') {
+        const nodeConnection = await util.promisify(visayasMindanaoNode.getConnection).bind(visayasMindanaoNode)();
+    } else {
+        const nodeConnection = await util.promisify(luzonNode.getConnection).bind(luzonNode)();
+    }
+
+    const query = util.promisify(nodeConnection.query).bind(nodeConnection);
+
+    await query('START TRANSACTION');
+
+    const log = { message: 'Starting Transaction', timestamp: new Date() };
+    addToLog(log);
+    console.log(log);
+
+    const result = await query('DELETE FROM test_table WHERE id = ?', id);
+
+    log = { message: 'Deleting', timestamp: new Date() };
+    addToLog(log);
+    console.log('Deleting data:', id);
+
+    await query('COMMIT');
+
+    console.log('Committing transaction');
+    log = { message: 'Committing Transaction', timestamp: new Date() };
+    addToLog(log);
+
+    nodeConnection.release();
+
+    if (result.affectedRows === 1) {
+        console.log('Data deleted successfully');
+        res.status(200).json({ message: 'Data deleted successfully' });
+    } else {
+        console.log('Failed to delete data');
+        res.status(500).json({ message: 'Failed to delete data' });
     }
 }
 
 // SERVER ROUTES
+/*
+    Add Appointment
 
+    Edit Appointment
+        - Region
+        - Type
+        - Status
+        - Virtual
+    Delete Appointment
+*/
+
+// LANDING ROUTE
 app.get('/', async (req, res) => {
     try {
         res.send('Hello World!');
         console.log('Hello World!');
+
+        // test database connections
         await testConnections();
+
+        // test logging system
         const log = { message: 'This is a log message', timestamp: new Date() };
         addToLog(log);
     } catch (error) {
@@ -118,53 +323,49 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.get('/write', async (req, res) => {
+// ADD APPOINTMENT
+app.get('/add', async (req, res) => {
     try {
-        const data = 1;
-        const secondaryNodeConnection = await util.promisify(secondaryNode.getConnection).bind(secondaryNode)();
-        const query = util.promisify(secondaryNodeConnection.query).bind(secondaryNodeConnection);
-        
-        // Start transaction
-        var log = { message: 'Starting Transaction', timestamp: new Date() };
-        addToLog(log);
-        console.log(log);
-        await query('START TRANSACTION');
-        
-        const result = await query('INSERT INTO test_table (test_column) VALUES (?)', (data));
-        log = { message: 'Inserting', timestamp: new Date() };
-        addToLog(log);
-        console.log('Inserting data:', data);
-        
-        // Commit transaction
-        console.log('Committing transaction');
-        await query('COMMT');
-        log = { message: 'Committing Transaction', timestamp: new Date() };
-        addToLog(log);
-        
-        secondaryNodeConnection.release();
-        
-        if (result.affectedRows === 1) {
-            console.log('Data inserted successfully');
-            res.status(200).json({ message: 'Data inserted successfully' });
-        } else {
-            console.log('Failed to insert data');
-            res.status(500).json({ message: 'Failed to insert data' });
-        }
+        await addDataToTable(req, res, 'central');
     } catch (error) {
+
+        // add try for other nodes
+
         console.error('Error:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
     console.log('Query uncommitted');
 });
 
-// do try catch in case of database failure
+// EDIT APPOINTMENT
+/*
+    - Region
+    - Type
+    - Status
+    - Virtual   
 
-app.get('/read', (req, res) => {
+*/
 
+app.patch('/edit/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { data } = req.body;
+        await updateDataInTable(req, res, 'central', id, data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
-app.post('/write', (req, res) => {
-    
+// DELETE APPOINTMENT
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await deleteDataFromTable(req, res, 'central', id);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 app.listen(port, () => {
